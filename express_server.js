6dd744ff7f00f54ca0262774c627c70efeb98ba3
Angela <liny0207@gmail.com>
2018-09-06 18:12:20 +0000

    users database added

--- express_server.js
+++ express_server.js
@@ -95,4 +95,15 @@ function generateRandomString() {
   return text;
 }
 
-
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
